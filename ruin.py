ac2e3e0f5f6ef94f9a4ba3f7a6d40e64364673b3
Joseph Suber <joesuber@gmail.com>
2017-07-10 14:50:52 -0500

    its vorking

--- ruin.py
+++ ruin.py
@@ -8,28 +8,44 @@ except:
     target = 600
     bet = 15
     rate_of_return = 0.98
-    print("using defaults for stake: ${}, target ${} and bet ${} and game return = {}"
-          .format(stake, target, bet, rate_of_return))
+
 
 trials = 10000
+
 
 def game(bet, rate_of_return=rate_of_return):
     return (bet + bet*rate_of_return) * randint(0,1)
 
 
-def ruin(stake=stake, target=target, bet=bet):
+def ruin(stake=stake, target=target, bet=bet, trials=trials, payout=rate_of_return):
     original_stake = stake
     wins = 0
+    promo = 0
     for t in range(trials):
         while ((stake - bet) > 0) and (stake < target):
+            promo += bet
             stake -= bet
             stake += game(bet)
         if stake > original_stake:
             wins += 1
         stake = original_stake
-    print("{}/{} ({:d2}) wins of greater than ${} (at {} payout ratio)"
-          .format(wins, trials, wins/float(trials), target, rate_of_return))
+    promo = promo / float(trials)
+    print("{:.2f}%  wins going from ${} to ${} with bets of {}   ({:.2f} payout, {} avg. promo bucks)"
+          .format(100*(wins/float(trials)), stake, target, bet, payout, rate_of_return, promo))
+
+stakes = [300, 500]
+targets = [550, 650, 750, 1000, 2000]
+bets = [5, 15, 25, 75]
+payouts = [0.985, 0.995]
+
+
+def bigone():
+    for stake in stakes:
+        for target in targets:
+            for bet in bets:
+                for payout in payouts:
+                    ruin(stake=stake, target=target, bet=bet, trials=trials, payout=payout)
 
 
 if __name__ == "__main__":
-    ruin()
+    bigone()
