94d78894e59c0459e583ca166a7ecdaf1c4ab740
Joseph Suber <joesuber@gmail.com>
2017-07-13 16:06:33 -0500

    frustration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import py2exe
+
+setup(console=['dialer.py'])
+
+""" 
+may need to download VC redistrutable 
+https://www.microsoft.com/en-us/download/details.aspx?id=29 
+
+tutorial:
+
+http://www.py2exe.org/index.cgi/Tutorial
+"""
