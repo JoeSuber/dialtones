049036f96a6f121a88abd96cef2c2e18af7024bd
Joseph Suber <joesuber@gmail.com>
2017-07-31 15:29:34 -0500

    psu

--- lizardhide.py
+++ lizardhide.py
@@ -22,10 +22,32 @@ take screenshot
 
 -- parse results --
 """
-
-
-
+import subprocess
+import time
+import sys
+import os
 from chamcodes import dial_codes
+
+
+def devicelist():
+    """ return a list of device codes for the usb-plugged-in devices
+     in the order they are given by calling 'adb devices' """
+    raw = subprocess.run(["adb", "devices"], stdout=subprocess.PIPE).stdout.decode("utf-8").split(os.linesep)[1:]
+    return [entry.split('\t')[0] for entry in raw if "\t" in entry]
+
+
+def carrier_detect():
+    devices = devicelist()
+    # check service state
+    for device in devices:
+        cmds = ['adb', '-s', '{}'.format(device), 'wait-for-device', 'shell', 'dumpsys', 'telephony.registry']
+        raw = subprocess.run(cmds, stdout=subprocess.PIPE).stdout.decode("utf-8").split(os.linesep)
+        print("*********************************")
+        entry = [blurb for blurb in raw if "sprint" in blurb.lower()]
+        for e in entry:
+            if e:
+                print(e)
+
 
 if __name__ == "__main__":
     print(dial_codes['All'])
